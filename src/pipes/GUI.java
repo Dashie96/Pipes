3a1dde951bd9b5a74030c3f407e82a648d02aa49
Eric Slaweski <Dashie96@users.noreply.github.com>
2014-06-24 11:51:03 -0400

    Added the "Licence Agreement"

--- src/pipes/GUI.java
+++ src/pipes/GUI.java
@@ -4,7 +4,7 @@ package pipes;
 /**
  *  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
- * <Eric.Slaweski@gmail.com> wrote this file.As long as you retain this notice 
+ * <Eric.Slaweski@gmail.com> wrote this file. As long as you retain this notice 
  * you can do whatever you want with this stuff. If we meet some day, and you 
  * think this stuff is worth it, you can buy me a beer in return. Eric Slaweski
  * ----------------------------------------------------------------------------
