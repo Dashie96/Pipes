b230a11e7b6cdde031f1b806cc2b5fbab47ba527
Eric Slaweski <Dashie96@users.noreply.github.com>
2014-06-24 11:53:52 -0400

    Added the "Licence Agreement"

--- src/pipes/Item.java
+++ src/pipes/Item.java
@@ -1,7 +1,12 @@
 package pipes;
 
 /**
- * Write a description of class Items here.
+ * ---------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <Eric.Slaweski@gmail.com> wrote this file. As long as you retain this notice 
+ * you can do whatever you want with this stuff. If we meet some day, and you 
+ * think this stuff is worth it, you can buy me a beer in return. Eric Slaweski
+ * ----------------------------------------------------------------------------
  * 
  * @author (Eric Slaweski) 
  * @version (5/3/14)
