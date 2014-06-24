2890e34f48070b3ddaacf7d53b90f3a29d61faa9
Eric Slaweski <Dashie96@users.noreply.github.com>
2014-06-24 11:47:50 -0400

    Added the "Licence Agreement"

--- src/pipes/GUI.java
+++ src/pipes/GUI.java
@@ -2,7 +2,13 @@ package pipes;
 
 
 /**
- *
+ *  * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <Eric.Slaweski@gmail.com> wrote this file.As long as you retain this notice 
+ * you can do whatever you want with this stuff. If we meet some day, and you 
+ * think this stuff is worth it, you can buy me a beer in return. Eric Slaweski
+ * ----------------------------------------------------------------------------
+ * 
  * @author Eric Slaweski
  */
 public class GUI extends javax.swing.JFrame
